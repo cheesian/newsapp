4b810b36ac6298f03f2f4c7a1e7745933e804fe1
Ian M <ian@dataintegrated.co.ke>
2020-06-04 09:29:31 +0300

    Addeed endpoints for sign up and sign in

---
+++ app/src/main/java/com/example/news/data/request/SignUpApiService.kt
@@ -0,0 +1,15 @@
+package com.example.news.data.request
+
+import com.example.news.data.request.URLs.SIGN_UP_ENDPOINT
+import com.google.gson.annotations.SerializedName
+import retrofit2.http.POST
+
+
+/**
+Created by ian
+ */
+
+interface SignUpApiService {
+
+
+}
