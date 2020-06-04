204e2409c0995fe7bbf11db2695241f9db72a892
Ian M <ian@dataintegrated.co.ke>
2020-06-04 09:34:13 +0300

    Added request body for sign up

--- app/src/main/java/com/example/news/data/request/SignUpApiService.kt
+++ app/src/main/java/com/example/news/data/request/SignUpApiService.kt
@@ -1,6 +1,7 @@
 package com.example.news.data.request
 
 import com.example.news.data.request.URLs.SIGN_UP_ENDPOINT
+import com.example.news.data.request.signUp.SignUpRequest
 import com.google.gson.annotations.SerializedName
 import retrofit2.http.POST
 
@@ -10,6 +11,8 @@ Created by ian
  */
 
 interface SignUpApiService {
-
-
+    @POST(SIGN_UP_ENDPOINT)
+    fun signUp(
+        data: SignUpRequest
+    )
 }
