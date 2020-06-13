650e6bff7393d123a1b86c327d6e944e8851da25
Ian M <ian@dataintegrated.co.ke>
2020-06-13 12:54:35 +0300

    Added response body for signup

--- app/src/main/java/com/example/news/data/request/SignUpApiService.kt
+++ app/src/main/java/com/example/news/data/request/SignUpApiService.kt
@@ -2,7 +2,9 @@ package com.example.news.data.request
 
 import com.example.news.data.request.URLs.SIGN_UP_ENDPOINT
 import com.example.news.data.request.signUp.SignUpRequest
+import com.example.news.data.response.signUp.SignUpResponse
 import com.google.gson.annotations.SerializedName
+import io.reactivex.Observable
 import retrofit2.http.POST
 
 
@@ -14,5 +16,5 @@ interface SignUpApiService {
     @POST(SIGN_UP_ENDPOINT)
     fun signUp(
         data: SignUpRequest
-    )
+    ): Observable<SignUpResponse>
 }
